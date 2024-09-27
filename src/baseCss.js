365c3baf432afa5acb4b778c8058d0140eea547c
Kenan Gundogan <kenangundogann@gmail.com>
2024-09-27 14:32:40 +0300

    Refactor generateBaseCss() to use extractedClasses and allClasses

--- src/baseCss.js
+++ src/baseCss.js
@@ -1,11 +1,11 @@
 // Statik class'ları CSS'e dönüştürme ve harf sırasına göre sıralama
-export function generateBaseCss(classes, baseClasses) {
-    return classes
+export function generateBaseCss(extractedClasses, allClasses) {
+    return extractedClasses
         .filter(className => !className.includes(':')) // Responsive class'ları hariç tut
         .sort() // Harf sırasına göre sıralama
         .map(className => {
-            if (baseClasses[className]) {
-                return `.${className} { ${baseClasses[className]} }`; // Statik class'ı ekle
+            if (allClasses[className]) {
+                return `.${className} { ${allClasses[className]} }`; // Statik class'ı ekle
             }
             return null;
         })
