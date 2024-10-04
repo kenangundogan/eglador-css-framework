2eb004bc684dac3170f238f10d8585e1cd99606c
Kenan Gündoğan <19374764+kenangundogan@users.noreply.github.com>
2024-10-04 16:16:04 +0300

    Refactor importantCss.js to handle custom classes and optimize code

--- src/generate/importantCss.js
+++ src/generate/importantCss.js
@@ -1,12 +1,35 @@
 import { escapeClassName } from '../utils/escapeClassName.js';
+import { prefixToPropertyMap } from './../properties/prefixToPropertyMap.js';
 
 export function importantCss(important, allClasses) {
     const importantCss = important.map(className => {
         const baseClass = className.split('!')[1];
-        console.log("baseClass", baseClass);
+
+        // eğer baseClass içinde custom bir sınıf varsa
+        if (baseClass.includes('[') && baseClass.includes(']')) {
+            const match = baseClass.match(/([a-zA-Z-]+)\[(.+)\]/);
+            const prefix = match[1].replace(/^-|-$/g, '');
+            const value = match[2];
+            console.log('prefix', prefix, 'value', value, 'prefix', prefix);
+
+            // prefixToPropertyMap içinde prefix var mı kontrol et
+            if (prefixToPropertyMap[prefix]) {
+                const cssProperty = prefixToPropertyMap[prefix];
+                let cssRule = `${cssProperty}: ${value}`;
+
+                // Eğer 'calc' veya 'max' fonksiyonları içeriyorsa virgülleri kaçış karakteriyle değiştirelim
+                if (value.includes('calc') || value.includes('max')) {
+                    cssRule = cssRule.replace(/,/g, '\\2c '); // Virgülleri \2c ile değiştir
+                    cssRule = cssRule.replace(/([+-])/g, ' $1 '); // + ve - işaretleri arasına boşluk ekle
+                }
+
+                return `.${escapeClassName(className)} { ${cssRule} !important; }`;
+            }
+
+        }
         if (allClasses[baseClass]) {
-            console.log("baseClass", allClasses[baseClass]);
-            return `.${escapeClassName(className)} { ${allClasses[baseClass]} }`;
+            const cssRule = allClasses[baseClass].replace(/;/g, '');
+            return `.${escapeClassName(className)} { ${cssRule} !important; }`;
         }
     });
 
