e1ab7f673b23dcf174de61dea35462be3e6591cc
Kenan Gundogan <kenangundogann@gmail.com>
2024-10-19 15:17:51 +0300

    Refactor import path in resetCss.js to use _reset.js

--- src/generate/resetCss.js
+++ src/generate/resetCss.js
@@ -1,5 +1,5 @@
 import config from '../eglador.config.js';
-import { generateResetClasses } from '../properties/reset.js';
+import { generateResetClasses } from '../properties/_reset.js';
 
 export function resetCss() {
     if (config.cssreset) {
--- src/properties/letterSpacing.js
+++ src/properties/letterSpacing.js
@@ -13,16 +13,12 @@ export function generateLetterSpacingClasses() {
     Object.keys(letterSpacingValues).forEach(key => {
         const value = letterSpacingValues[key];
 
-        // Pozitif letter-spacing sınıfları
         classes[`tracking-${key}`] = `letter-spacing: ${value};`;
 
-        // Negatif letter-spacing sınıfları
         if (value.startsWith('-')) {
-            // Eğer negatifse, pozitif hale getiriyoruz
-            const positiveValue = value.slice(1); // "-" işaretini kaldırıyoruz
+            const positiveValue = value.slice(1);
             classes[`-tracking-${key}`] = `letter-spacing: ${positiveValue};`;
         } else {
-            // Eğer pozitifse, negatif hale getiriyoruz
             const negativeValue = `-${value}`;
             classes[`-tracking-${key}`] = `letter-spacing: ${negativeValue};`;
         }
