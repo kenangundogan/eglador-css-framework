ecf57d1c969d850383e4216a48b44970a49de2a2
Kenan Gundogan <kenangundogann@gmail.com>
2024-10-19 13:27:00 +0300

    Refactor escapeClassName.js and sanitizeValue.js to optimize code and remove unnecessary comments and unused code

--- src/utils/formatMathExpressions.js
+++ src/utils/formatMathExpressions.js
@@ -21,11 +21,9 @@ const mathFunctions = [
 ];
 
 export function formatMathExpressions(value) {
-    // Eğer value içinde mathFunctions'tan biri varsa, virgülleri ve operatörleri düzenleyelim
     const hasMathFunction = mathFunctions.some(func => value.startsWith(func));
 
     if (hasMathFunction) {
-        // matematik işaretlerinin arasına boşluk ekle
         let formattedValue = value.replace(/([+\-*/])/g, ' $1 ');
         return formattedValue;
     }
