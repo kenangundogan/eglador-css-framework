75e104a9e5f66b45a91f516c1aa4e5a7468f595a
Kenan Gündoğan <19374764+kenangundogan@users.noreply.github.com>
2024-10-31 16:51:27 +0300

    Refactor readCssFile function to include Turkish translations for error messages

--- src/read/readCssFile.js
+++ src/read/readCssFile.js
@@ -3,7 +3,7 @@ import pc from 'picocolors';
 export function readCssFile(project) {
     const cssContent = fs.readFileSync(project.input, 'utf8');
     if (!cssContent) {
-        console.log(pc.red('Error: ') + 'Error reading CSS file at ' + pc.red(project.input));
+        console.log(pc.red('Hata: ') + 'CSS dosyası okunurken hata oluştu ' + pc.red(project.input));
         return '';
     }
     return cssContent;
